d5f5d32062de7e7548d0de533b69727c1f2d49dd
Fabrice Matrat <fabricematrat@gmail.com>
2015-09-09 08:35:57 +0200

    Fix for phantom support of Float64Array.

--- templates/yui-header.js
+++ templates/yui-header.js
@@ -1,4 +1,8 @@
 /*jslint node: true, continue: true, eqeq: true, forin: true, nomen: true, plusplus: true, todo: true, vars: true, white: true */
+if (typeof Float64Array === "undefined") {
+  Float64Array = Array
+}
+
 'use strict';
 
 YUI.add("macaroon", function(Y) {
