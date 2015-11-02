2f72f7bbca7a7b2dd27fdbabfb8addf581f4491c
Fabrice Matrat <fabricematrat@gmail.com>
2015-11-02 18:00:12 +0100

    Float64Array polyfill start.

--- templates/yui-header.js
+++ templates/yui-header.js
@@ -1,6 +1,9 @@
 /*jslint node: true, continue: true, eqeq: true, forin: true, nomen: true, plusplus: true, todo: true, vars: true, white: true */
 if (typeof Float64Array === "undefined") {
-  Float64Array = Array
+  Float64Array = Array;
+  Float64Array.prototype.subarray = function(start, end) {
+    return this.slice(start, end);
+  }
 }
 
 'use strict';
